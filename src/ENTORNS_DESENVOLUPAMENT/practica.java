b7f7351b1ba3a87f3d6b1f6d3909fb37a8404bb3
gblancooo <gerardblanco2006@gmail.com>
2024-11-11 16:25:19 +0100

    Practica1

---
+++ src/ENTORNS_DESENVOLUPAMENT/practica.java
@@ -0,0 +1,30 @@
+package ENTORNS_DESENVOLUPAMENT;
+
+public class practica {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] sumArray = {4,36,4,23,867};
+		int[] subArray = {785675, 56, 5675, 56544, 234};
+		int[] prodArray = {6, 8, 20, 23, 12};
+		
+		// User 1 fer la suma dels elements de sumArray
+		
+		int suma = 0;
+		for (int vueltas = 0; vueltas < prodArray.length; vueltas++) {
+			
+			suma += sumArray[vueltas];
+		}
+		
+		System.out.println("Total: " + suma + "");
+		
+		// User 2 fer la resta dels elements de subArray
+		
+		
+		
+		// User 3 fer la multiplicació dels elements de prodArray
+		
+		
+	}
+
+}
